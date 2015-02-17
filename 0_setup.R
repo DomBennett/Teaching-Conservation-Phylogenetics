03a7eb608b16ee401a1dcae5e2326c7bd5ee4e13
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-02-17 16:10:46 +0000

    Init

---
+++ 0_setup.R
@@ -0,0 +1,9 @@
+# 17/02/2015
+# Installing the packages
+
+# MoreTreeTools
+install.packages ('devtools')
+library ('devtools')
+install_github ('https://github.com/DomBennett/MoreTreeTools.git')
+
+
