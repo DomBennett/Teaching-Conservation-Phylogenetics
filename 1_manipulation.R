251f890c027690c64f3e4b625f811c40fb6182d5
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-02-20 18:27:48 +0000

    Finished answers

--- 1_manipulation.R
+++ 1_manipulation.R
@@ -9,7 +9,7 @@ library (MoreTreeTools)
 input.dir <- file.path ('data', 'Orre_data')
 tree <- read.tree (file.path (input.dir, 'tree.tre'))
 
-# EXPLORING TREE STRUCTURE
+# EXPLORING TREE 1
 str (tree)  # composed of 4 slots
 tree$Nnode  # number of internal nodes
 tree$tip.label  # labels of tips
@@ -34,3 +34,29 @@ ltt.plot (tree)  # lineages through time, this is a community plot
  # it can't tell us much about the evolution of the clade
 
 # MANIPULATING TREE
+plot (drop.tip (tree, tip = sample (tree$tip.label, 1)))  # one fewer tip
+sample.tips <- sample (tree$tip.label, round (length (tree$tip.label) * 0.1))  # random tips
+sample.tree <- drop.tip (tree, tip = tree$tip.label[!tree$tip.label %in% sample.tips])
+plot (sample.tree)  # smaller tree!
+sample.tips2 <- sample (tree$tip.label, round (length (tree$tip.label) * 0.1))  # random tips
+sample.tree2 <- drop.tip (tree, tip = tree$tip.label[!tree$tip.label %in% sample.tips2])
+calcDist (sample.tree, sample.tree2)  # too many tips aren't shared
+clade <- extract.clade (tree, node=110)
+clade.parent <- tree$edge[which (tree$edge[ ,2] == 110), 1]
+tree$edge[which (tree$edge[ ,1] == clade.parent), 2] # 189 is the sister
+
+# EXPLORING TREE 2
+# let's look at the sample.tree, it's easier to see what's going on
+ttt.dists <- cophenetic.phylo (sample.tree)  # tip to tip distances
+ntr.dists <- vcv.phylo (sample.tree)  # node to root distances
+which (colSums (ttt.dists == max (ttt.dists)) > 0)  # most distant tips
+mean (diag (ntr.dists))  # tree age i.e. the mean root to tip distance
+max (diag (ntr.dists))
+min (diag (ntr.dists))
+tree$tip.label[which.max (calcED (tree)[ ,1])]  # most ED species
+# and for different ED metrics ...
+tree$tip.label[which.max (calcED (tree, type='ES')[ ,1])]
+tree$tip.label[which.max (calcED (tree, type='PE')[ ,1])]
+
+# CALCPD
+# See tools.R
